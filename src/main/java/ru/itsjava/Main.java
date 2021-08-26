23330464d8c41752921b07d63b45b19469705c60
Oleg <oleg.kholyavchuk@bk.ru>
2021-08-26 21:06:11 +0300

    26_08_21

--- src/main/java/ru/itsjava/Main.java
+++ src/main/java/ru/itsjava/Main.java
@@ -1,31 +1,15 @@
 package ru.itsjava;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+
+import ru.itsjava.servises.ClientService;
+import ru.itsjava.servises.ClientServiceImpl;
 
 public class Main {
 
-    public final static int PORT = 8081;
-    public final static String HOST = "localhost";
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws IOException {
-
-        Socket socket = new Socket(HOST, PORT);
-
-        if (socket.isConnected()){
-            PrintWriter serverWrite = new PrintWriter(socket.getOutputStream());
-            serverWrite.println("Hi from client");
-            serverWrite.flush();
-
-            BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            while (true){
-                System.out.println(serverReader.readLine());
-            }
-        }
-
+        ClientService clientService = new ClientServiceImpl();
+        clientService.start();
 
 
     }
---
+++ src/main/java/ru/itsjava/servises/MessageInputService.java
@@ -0,0 +1,6 @@
+package ru.itsjava.servises;
+
+public interface MessageInputService {
+
+    String getMessage();
+}
